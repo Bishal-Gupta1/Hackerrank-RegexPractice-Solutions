3af3a2ab513ea3cda30b86768cf457337c624dc4
Bishal Gupta <bishalgupta7@gmail.com>
2020-11-15 08:55:36 +0545

    Update Match Specific String.java

--- RegexPracitce_AllSolution/1_Introduction/Match Specific String.java
+++ RegexPracitce_AllSolution/1_Introduction/Match Specific String.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("[hackerank]"); 
+        tester.checker("hackerrank"); 
     
     }
 }
